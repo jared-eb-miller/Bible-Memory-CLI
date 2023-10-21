9c9f548ef627dd51d3be13f42fe154f8548cde54
Jared Miller <jebm423@gmail.com>
2023-10-20 22:08:04 -0400

    added cli.py

---
+++ cli.py
@@ -0,0 +1,12 @@
+def displayMenu():
+    print("""
+******************** Bible Memory CLI ********************
+
+    1) Re-synchronize local library
+    2) View statistics
+    3) Review / study verses
+
+**********************************************************
+""")
+
+displayMenu()
